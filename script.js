8d7b38092179aaa315bb6f51c4348b9daedda518
Maeinuddin khan <119691264+Maeinukhan63@users.noreply.github.com>
2024-08-31 12:15:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,25 @@ function convertToRoman(num) {
     };
 
   //your code here
+	
+    let result = '';
+
+    // Handle subtractive combinations (like 4, 9, 40, 90, etc.)
+    const romanPairs = [
+        ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400],
+        ['C', 100], ['XC', 90], ['L', 50], ['XL', 40],
+        ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]
+    ];
+
+    for (let i = 0; i < romanPairs.length; i++) {
+        const [roman, value] = romanPairs[i];
+        while (num >= value) {
+            result += roman;
+            num -= value;
+        }
+    }
+
+    return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
